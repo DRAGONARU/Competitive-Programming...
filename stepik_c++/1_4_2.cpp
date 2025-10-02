45ecd0748d1233b045b4441b2d1df175aa8e7065
DRAGONARU <138095702+DRAGONARU@users.noreply.github.com>
2025-10-02 23:14:45 +0500

    Create 1_4_2.cpp

---
+++ stepik_c++/1_4_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+  // put your code here
+  int a, b;
+  cin >> a >> b;
+  if (a > b)
+  {
+      cout << 1;
+  }
+  else if(a < b)
+  {
+      cout << 2;
+  }
+  else{
+      cout << 0;
+  }
+  return 0;
+}
