9688809192bfac99eba39d66e08a7e6334b8868c
DRAGONARU <138095702+DRAGONARU@users.noreply.github.com>
2025-10-04 21:44:23 +0500

    Create 1_4_9.cpp

---
+++ stepik_c++/1_4_9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int king_row, king_collum, pos_row, pos_collum;
+    cin >> king_row >> king_collum >> pos_row >> pos_collum;
+    if (abs(king_row - pos_row) ==  abs(king_collum - pos_collum) or king_row == pos_row or king_collum == pos_collum) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+	}
+    return 0;
+}
