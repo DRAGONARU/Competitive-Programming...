65c70044fa548a2ab649ecad79109ef1b2265691
DRAGONARU <138095702+DRAGONARU@users.noreply.github.com>
2025-10-05 21:02:50 +0500

    Create 1_4_11.cpp

---
+++ stepik_c++/1_4_11.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    if ((k % m == 0 or k % n == 0) and k <= n * m) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+	}
+    return 0;
+}
