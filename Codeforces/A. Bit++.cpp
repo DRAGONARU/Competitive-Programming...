51ba1c2fc6637bb65e7eb83bcae8d9631b845df8
DRAGONARU <138095702+DRAGONARU@users.noreply.github.com>
2025-10-07 16:14:34 +0500

    Create A. Bit++.cpp

---
+++ Codeforces/A. Bit++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 0, n;
+    string s;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s;
+        if (s == "X++" || s == "++X")
+            x++;
+        else if (s == "X--" || s == "--X")
+            x--;
+	}
+    cout << x;
+}
